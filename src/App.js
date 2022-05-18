4e3deb796c2bbf57fc8a3afefadcc52cc95b3fd3
anilamurali <anilamurali98@gmail.com>
2022-05-18 12:01:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Productentry from './Component/Productentry';
 import Poductsearch from './Component/Poductsearch';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Poductsearch/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element={<Productentry/>}/>
+      <Routea path='search' exact element={<Poductsearch/>}/>
+      <Route path='view' exact element={<Productview/>}/>
+    </Routes>
+    </BrowserRouter>
 
     </>
   );
