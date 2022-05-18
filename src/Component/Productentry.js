cc34d8b8bc465f2655200f43b88823b2b6f5c41f
anilamurali <anilamurali98@gmail.com>
2022-05-18 19:58:31 +0530

    third commit

--- src/Component/Productentry.js
+++ src/Component/Productentry.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const Productentry = () => {
+    
+    var [pname,setProduct]=useState("")
+    var [pdes,setDes]=useState("")
+    var [Price,setPrice]=useState("")
+    const changeData=()=>{
+        const data={"pname":pname,"descripton":pdes,"price":Price}
+        console.log(data)
+    }
   return (
     <div>
         <Navbar/>
@@ -11,18 +19,18 @@ const Productentry = () => {
             <div class="row g-2">
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">Product Name"</label>
-                    <input type="text" class="form-control"/>
+                    <input onChange={(a)=>{setProduct(a.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">Product Description</label>
-                    <input type="text" class="form-control"/>
+                    <input onChange={(a)=>{setDes(a.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">Product Price</label>
-                    <input type="text" class="form-control"/>
+                    <input onChange={(a)=>{setPrice(a.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button class="btn btn-success">ADD</button>
+                    <button onClick={changeData} class="btn btn-success">ADD</button>
                 </div>
             </div>
         </div>
