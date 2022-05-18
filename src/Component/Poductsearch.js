cfb7637678baf994f529a4aedf4a001a5298c74e
anilamurali <anilamurali98@gmail.com>
2022-05-18 10:34:16 +0530

    First Commit

--- src/Component/Poductsearch.js
+++ src/Component/Poductsearch.js
@@ -1,10 +1,14 @@
-<div class="container">
+import React from 'react'
+
+const Poductsearch = () => {
+  return (
+    <div><div class="container">
     <div class="row">
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div class="row">
+            <div class="row g-2">
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label for="" class="form-label">Product Name"</label>
-                    <input type="text" class="from-control">
+                    <label for="" class="form-label">Product Name</label>
+                    <input type="text" class="form-control"/>
                 </div>
                 
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -13,4 +17,8 @@
             </div>
         </div>
     </div>
-</div>
+</div></div>
+  )
+}
+
+export default Poductsearch
