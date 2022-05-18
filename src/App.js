150eee26bda96f30e861908664a38d84852b38d9
anilamurali <anilamurali98@gmail.com>
2022-05-18 11:11:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/cssbootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Productentry from './Component/Productentry';
+import Productview from './Component/Productview';
 
 
 
 function App() {
   return (
     <>
-    <Productentry/>
-
+    <Productview/>
     </>
   );
 }
